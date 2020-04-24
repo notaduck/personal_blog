9abe7e0e031cb0d381d71853d37ce67492f991f9
Daniel Guldberg Aaes <guldberg72@gmail.com>
2020-04-24 14:06:10 +0200

    node packages has been upgraded

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,6 +98,7 @@ module.exports = {
 			options: {
 				test: /\.js$|\.jsx$/,
 				exclude: /(node_modules|cache|public)/,
+				stages: ['develop'],
 				options: {
 					emitWarning: true,
 					failOnError: false
